9e8b495c10dc1166151d1c777dfeaf237dc32fab
mdarifk77 <mdarifkarim360@gmail.com>
2026-06-18 18:02:07 +0530

    left rotate of array by one place

---
+++ Arrays/src/RotateByOne.java
@@ -0,0 +1,20 @@
+public class RotateByOne {
+    public static void  leftRotate(int [] arr){
+
+        int temp = arr[0];
+
+        for( int i = 1; i < arr.length; i++){
+            arr[i - 1] = arr[i];
+        }
+        arr[arr.length-1] = temp;
+
+    }
+
+    public static void main(String[] args) {
+        int [] arr = {1,2,3,4,5,6};
+        leftRotate(arr);
+        for(int num : arr){
+            System.out.print(num+ " ");
+        }
+    }
+}
