9a1e609be00c02c1c243d0e1468bb7e6b5678370
mdarifk77 <mdarifkarim360@gmail.com>
2026-06-27 02:46:51 +0530

    added in a sorted array find a missing number

--- Arrays/src/MissingNumber.java
+++ Arrays/src/MissingNumber.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
+
+// Brute Force solution:
 
 public class MissingNumber {
+    public static int missingNumber( int [] arr, int N){
+
+        for (int i = 1; i<N; i++){
+            boolean found = false;
+
+            for( int j =0; j<arr.length; j++){
+                if(arr[j] == i ) {
+                    found = true;
+                    break;
+                }
+
+            }
+            if( !found){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int [] arr = {1,2,3,5,7,8};
+        System.out.println(missingNumber(arr,6));
+    }
+
 }
