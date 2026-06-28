9fb7e4fe0c686b3e05469571a0af6e4707195ab9
mdarifk77 <mdarifkarim360@gmail.com>
2026-06-29 00:45:52 +0530

    find the missing number in the given array

--- Arrays/src/MissingNumber2.java
+++ Arrays/src/MissingNumber2.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+// Optimal Approach sum method:
 
 public class MissingNumber2 {
+    public static int missingNumber(int [] arr, int N){
+
+        int expectedSum = N * (N+1) / 2;
+        int actualSum = 0;
+
+        for(int num = 0; num < arr.length; num++){
+            actualSum += arr[num];
+
+        }
+        return expectedSum - actualSum;
+    }
+
+    public static void main(String[] args) {
+        int [] arr = { 1, 2, 4, 5, 6};
+        System.out.println(missingNumber(arr , 6));
+    }
 }
