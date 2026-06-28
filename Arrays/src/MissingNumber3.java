768d50f65f08801688f0602d72aa8b25019d9f3a
mdarifk77 <mdarifkarim360@gmail.com>
2026-06-29 00:47:09 +0530

    find the missing number in the given array

--- Arrays/src/MissingNumber3.java
+++ Arrays/src/MissingNumber3.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+// Optimal Approach xor method:
 
 public class MissingNumber3 {
+    public static int missingNumber(int [] arr , int N){
+
+        int xor1 = 0;
+        int xor2 = 0;
+
+        for (int i = 1; i <= N; i++){
+            xor1 ^= i;
+        }
+        for (int j = 0; j < arr.length; j++){
+            xor2 ^= arr[j];
+        }
+        return xor1 ^ xor2 ;
+    }
+
+    public static void main(String[] args) {
+        int [] arr = {1,2,3,5,6};
+        System.out.println(missingNumber(arr,6));
+    }
 }
